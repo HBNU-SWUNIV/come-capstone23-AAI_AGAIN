06e0bc57c349065f69ee7de9994724fecb8960b2
choics0057 <121301088+choics0057@users.noreply.github.com>
2023-10-30 16:19:12 +0900

    Add files via upload

---
+++ merge_bash.sh
@@ -0,0 +1,5 @@
+python merge_llama2_with_chinese_lora_low_mem.py \
+    --base_model Chang-Su/llama-2-13b-chat-ko \
+    --lora_model your_folder \
+    --output_type huggingface \
+    --output_dir /data02/MLP/cschoi/Chinese-LLaMA-Alpaca-2/bllossom-1.0-13b
