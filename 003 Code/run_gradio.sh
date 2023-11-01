64f3c24e72f19d2c3c2562c248f79ec2743c559c
hyeonseok <87563087+lhsstn@users.noreply.github.com>
2023-11-01 11:58:19 +0900

    Delete 003 Code directory

--- 003 Code/run_gradio.sh
+++
@@ -1,7 +0,0 @@
-CUDA_VISIBLE_DEVICES=6 python ./gradio_demo.py \
-    --base_model Chang-Su/llama-2-13b-chat-ko \
-    --tokenizer_path Chang-Su/llama-2-13b-chat-ko \
-    --lora_model your_lora_folder \
-    --port 11113
-    # --max_memory 2048
-    
