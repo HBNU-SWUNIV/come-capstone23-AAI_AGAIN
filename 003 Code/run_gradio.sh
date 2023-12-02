8fac41738ab2ceccc73a181afb9f4ba5b11a29b5
hyeonseok <87563087+lhsstn@users.noreply.github.com>
2023-12-02 19:17:10 +0900

    Update run_gradio.sh

--- 003 Code/run_gradio.sh
+++ 003 Code/run_gradio.sh
@@ -1,7 +1,7 @@
-CUDA_VISIBLE_DEVICES=6 python ./gradio_demo.py \
-    --base_model Chang-Su/llama-2-13b-chat-ko \
-    --tokenizer_path Chang-Su/llama-2-13b-chat-ko \
-    --lora_model your_lora_folder \
-    --port 11113
+CUDA_VISIBLE_DEVICES=3 python /home/hslim/scripts/inference/gradio_demo.py \
+    --base_model Teddysum/bllossom-1.01-13b \
+    --tokenizer_path Teddysum/bllossom-1.01-13b
+    # --port 29542
     # --max_memory 2048
+    #     --lora_model /data02/MLP/cschoi/Chinese-LLaMA-Alpaca-2/backup/with_kr_and_en/sft_lora_model \
     
